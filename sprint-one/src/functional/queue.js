24af41dd9e9333387cf6640a9f0222770b5200c9
Lawrence Malloy <lcmalloy@gmail.com>
2022-02-24 17:48:14 -0800

    Completed Queue.js

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -6,14 +6,35 @@ var Queue = function() {
 
   // Implement the methods below
 
+  var index = 0;
+  var returnIndex = 0;
+
   someInstance.enqueue = function(value) {
+    //enqueue adds a value to the back
+    storage[index] = value;
+    index++;
   };
 
   someInstance.dequeue = function() {
+    //dequeue removes value from the front
+    if (index > 0) {
+      index--;
+      storage[returnIndex];
+      returnIndex++;
+    }
+
   };
 
   someInstance.size = function() {
+    //We need a counter that will increase when we enqueue and decrease when we dequeue
+    return index;
   };
 
   return someInstance;
 };
+
+
+
+
+
+
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,15 +4,27 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var index = 0;
+
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[index] = value;
+    index++
+
   };
 
   someInstance.pop = function() {
+      if (index > 0) {
+        index--
+      }
+      return storage[index];
   };
 
   someInstance.size = function() {
+    return index;
+
   };
 
   return someInstance;
 };
+
